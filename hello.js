d09331b2e697ba84e965eb68d6e90e5c0d503720
Azaki <zakidawud69@gmail.com>
2025-11-04 21:28:28 +0700

    Dasar

--- hello.js
+++ hello.js
@@ -1,16 +1,3 @@
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <title>Hello World</title>
-// </head>
-// <body>
-// <script>
-//     document.write("Hello, World!")
-// </script>
-// </body>
-// </html>
-
-// Program di atas membuat hello dengan JavaScript dalam HTML sederhana
-
 document.writeln("Hello, World!");
+
+// Program Javascript ini ditampilkan di HTML.
