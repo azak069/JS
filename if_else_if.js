14812c55b4f0ef35a9404d9c923dd7e21eaa98a6
Azaki <zakidawud69@gmail.com>
2025-11-16 14:33:38 +0700

    Dasar

---
+++ if_else_if.js
@@ -0,0 +1,29 @@
+const nilai1 = 90;
+const nilai2 = 80;
+const nilai3 = 70;
+
+if (nilai1 < 75){
+    console.log("Coba Lagi");
+} else if (nilai1 < 85) {
+    console.log("Cukup Baik")
+} else {
+    console.log("Hebat")
+}
+
+if (nilai2 < 75){
+    console.log("Coba Lagi");
+} else if (nilai2 < 85) {
+    console.log("Cukup Baik")
+} else {
+    console.log("Hebat")
+}
+
+if (nilai3 < 75){
+    console.log("Coba Lagi");
+} else if (nilai3 < 85) {
+    console.log("Cukup Baik")
+} else {
+    console.log("Hebat")
+}
+
+// Program ini mendemonstrasikan penggunaan kontrol alur program if else if
