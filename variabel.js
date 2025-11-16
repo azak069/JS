e831d42f9a56fdebd554dacc4eee3d958104b073
Azaki <zakidawud69@gmail.com>
2025-11-16 09:29:56 +0700

    Dasar

---
+++ variabel.js
@@ -0,0 +1,14 @@
+var namaLengkap = "Albert Camus"; // dulu menggunakan var
+namaLengkap = "Abdurahman Zaki";
+namaLengkap = "Emil" + " " + "Cioran";
+let nama = "Albert"; // sekarang menggunakan let
+nama = "Zaki";
+nama = "Emil";
+const namaBelakang = "Devil";
+// namaBelakang = "Angel"; // const tidak bisa diubah
+console.info(namaLengkap);
+console.info(nama);
+console.info("Nama : " + nama);
+console.info(namaBelakang);
+
+// Program ini mendemonstrasikan penggunaan variabel dalam javascript
