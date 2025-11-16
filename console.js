f7669edeee7e6542f342488efb536f2b2be6f65d
Azaki <zakidawud69@gmail.com>
2025-11-16 09:38:52 +0700

    Dasar

---
+++ console.js
@@ -0,0 +1,15 @@
+let sapa = "Hallo!";
+console.info(sapa);
+sapa = "Hey!";
+console.log(sapa);
+
+let bug = "Perbaiki!";
+console.debug(bug)
+
+let ping = "Peringatan!";
+console.warn(ping);
+
+let er = "Error!";
+console.error(er);
+
+// Program ini menampilkan penggunaan console sesuai tingkatan dalam javascript
