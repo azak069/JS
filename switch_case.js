ba1b9324c168ef8662e8187c7f88bf9d9dab6547
Azaki <zakidawud69@gmail.com>
2025-11-17 18:18:48 +0700

    Dasar

---
+++ switch_case.js
@@ -0,0 +1,20 @@
+const nilai = 'E';
+
+switch (nilai){
+    case 'A':
+        console.info("Luar Biasa King!");
+        break;
+    case 'B':
+        console.info("Bagus Boss!")
+        break;
+    case 'C':
+        console.info("B aja")
+        break;
+    case 'D':
+        console.info("Belajar lagi dek!")
+        break;
+    default:
+        console.info("Masih ada opsi pindah jurusan bang!")
+}
+
+// Program ini mendemonstrasikan penggunaan switch case sebagai alternatif if else if
