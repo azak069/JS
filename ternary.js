141607009d9e2552ce1d62760acb3b7d97a44e00
Azaki <zakidawud69@gmail.com>
2025-11-17 19:35:36 +0700

    Dasar

---
+++ ternary.js
@@ -0,0 +1,6 @@
+const nilai = 90;
+const lulus = nilai >= 75 ? "Hebat Anda Lulus!" : "Maaf Coba Lagi";
+
+console.info(lulus)
+
+// Program ini memperlihatkan penggunaan ternary operator sebagai alternatif dari if else untuk kasus sederhana 
