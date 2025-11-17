68c0de11fed2d6c93d6b937e6826b2fb25dd9a1f
Azaki <zakidawud69@gmail.com>
2025-11-17 18:29:47 +0700

    Dasar

---
+++ typeof.js
@@ -0,0 +1,15 @@
+const nama = "Zaki";
+const umur = 20;
+const status = null;
+const kerja = undefined;
+let hobi;
+const bisa = true;
+
+console.info(typeof nama); // string
+console.info(typeof umur); // number
+console.info(typeof status); // object
+console.info(typeof kerja); // undifined
+console.info(typeof hobi); // undifined
+console.info(typeof bisa); // boolean
+
+// Program ini mendemonstrasikan penggunaan typeof untuk mengecek tipe data dari nilai
