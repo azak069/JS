ccefba0ca55792387af4fb6f920ecc82019d70a4
Azaki <zakidawud69@gmail.com>
2025-11-18 17:04:55 +0700

    Dasar

---
+++ for_loop.js
@@ -0,0 +1,11 @@
+let p = [];
+
+for (let i = 0; i < 10; i++){
+    p[i] = i + 1;
+}
+
+for (let i = 0; i < 10; i++){
+    console.info(p[i]);
+}
+
+// Program ini mendemonstrasikan penggunaan for loop untuk melakukan perulangan
