1076fa30dab4e10ca5f0bab5a1d510808445c376
Azaki <zakidawud69@gmail.com>
2025-11-19 11:38:49 +0700

    Dasar

---
+++ for_of.js
@@ -0,0 +1,13 @@
+const orang = ["Davy", "D", "Jones"];
+
+for (const nama of orang){
+    console.info(nama);
+}; // mengakses semua nilai dalam array(bukan indeks) dengan perulangan
+
+const namaLengkap = "Davy D Jones";
+
+for (const kar of namaLengkap){
+    console.info(kar);
+}; // bisa mengakses karakter dalam string langsung
+
+// Program ini mendemonstrasikan penggunaan perulangan for of untuk mengakses nilai
