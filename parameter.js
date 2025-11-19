3d9276f3e91484b0bba17f864348a021c8a949fd
Azaki <zakidawud69@gmail.com>
2025-11-19 12:22:03 +0700

    Dasar

---
+++ parameter.js
@@ -0,0 +1,8 @@
+function sapa(namaDepan, namaBelakang){
+    console.info(`Halo ${namaDepan} ${namaBelakang}! Senang bertemu denganmu.`)
+}; // membuat function beserta parameternya
+
+sapa("Abdurahman", "Zaki"); // memanggil function dan memasukkan parameter
+sapa("azak", 69); // nilai bebas, bisa string, number, dll.
+
+// Program ini mendemonstrasikan penggunaan parameter/argument dalam function untuk memasukkan nilai yang bisa dikelola funtion
