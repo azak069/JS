af519d2d38803c8a0e0b63f1e03ced4b3e4d9363
Azaki <zakidawud69@gmail.com>
2025-11-19 20:21:39 +0700

    Dasar

--- parameter.js
+++ parameter.js
@@ -4,5 +4,8 @@ function sapa(namaDepan, namaBelakang){
 
 sapa("Abdurahman", "Zaki"); // memanggil function dan memasukkan parameter
 sapa("azak", 69); // nilai bebas, bisa string, number, dll.
+sapa("azak"); // walau parameter tidak diisi, javascript tidak akan error, tapi jadi undefined
+sapa();
+sapa("Abdurahman", "Zaki", "D"); // bahkan tidak error mengisi nilai melebihi parameter, tapi tidak dibaca
 
 // Program ini mendemonstrasikan penggunaan parameter/argument dalam function untuk memasukkan nilai yang bisa dikelola funtion
