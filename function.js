99c1384d54c976b27fa632e8650ad3e23b0827fd
Azaki <zakidawud69@gmail.com>
2025-11-19 12:15:02 +0700

    Dasar

---
+++ function.js
@@ -0,0 +1,8 @@
+function hello(){
+    console.info("Hallo Dunia")
+}; // membuat function
+
+hello(); // memanggil function
+hello(); // bisa dipanggil terus
+
+// Program ini mendemonstrasikan pembuatan dan pemanggilan function
