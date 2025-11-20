a7ee460d63f77fc834fc4b31fdc052b24f499e72
Azaki <zakidawud69@gmail.com>
2025-11-20 19:57:49 +0700

    Dasar

--- parameter.js
+++ parameter.js
@@ -8,4 +8,14 @@ sapa("azak"); // walau parameter tidak diisi, javascript tidak akan error, tapi 
 sapa();
 sapa("Abdurahman", "Zaki", "D"); // bahkan tidak error mengisi nilai melebihi parameter, tapi tidak dibaca
 
+function profil(nama, kelamin = "Tidak perlu tahu"){
+    console.info(`Nama saya ${nama}, gender saya ${kelamin}.`)
+}; // membuat parameter default yang akan dijalankan jika parameter tidak diisi
+
+profil("Zaki", "Laki-laki");
+profil("Abdurahman"); // "Tidak perlu tahu"
+profil("azak", undefined); // "Tidak perlu tahu"
+profil("Emil", null); // null
+profil(); // nama undefined
+
 // Program ini mendemonstrasikan penggunaan parameter/argument dalam function untuk memasukkan nilai yang bisa dikelola funtion
